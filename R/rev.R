a2bc83b4b8106ff466762e008a81df7e5a20c2ad
omaymaS <omayma.saeed@gmail.com>
2018-06-03 02:39:15 +0200

    update rev_signature() and rev_calls() documentation

--- R/rev.R
+++ R/rev.R
@@ -26,13 +26,16 @@ rev_fn_summary <- function(path = ".", igraph_obj = NULL){
   
 }
 
-#' Create a dataframe of functions that a called and called by
-#' 
-#' Create a dataframe of all functions in and used by the package
+#' Create a dataframe of all functions in a package with the count of functions each one calls and called by
 #' 
 #' @inheritParams rev_fn_summary
 #' 
-#' @return A dataframe of functions in and used by the package and the number of times they are called and called by
+#' @return A dataframe with the following columns:
+#' * f_name: function name 
+#' * called_by: number of other functions in the package that call this function directly
+#' * calls: number of other functions that are called by this function directly
+#' * exported: a boolean to indicate whether the function is exported or not
+#' @md
 #' 
 #' @export
 
@@ -65,13 +68,14 @@ rev_calls <- function(path = ".", igraph_obj = NULL){
   return(degree_df)
 }
 
-#' Extract functions arguments of a package
-#' 
-#' Extracts function arguments of all functions in a package into a dataframe
+#' Extracts arguments of all functions in a package into a dataframe
 #' 
 #' @inheritParams rev_fn_summary
 #' 
-#' @return A dataframe of function names 
+#' @return A dataframe with the following columns:
+#' * f_name: function name
+#' * f_args: function arguements
+#' @md
 #' 
 #' @export
 rev_signature <- function(path = "."){
