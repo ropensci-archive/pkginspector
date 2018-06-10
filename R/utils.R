34a32d8bc1663fa2dcff55743841b7dc6bb33f14
Joyce Robbins <jtr13@columbia.edu>
2018-06-09 22:20:32 -0400

    clean up, sort vertices by ID

--- R/utils.R
+++ R/utils.R
@@ -8,26 +8,40 @@ check_if_installed <- function(package){
 
 #' @noRd
 
-create_package_igraph <- function(path = ".", include_base = FALSE, directed = TRUE,
-                                  external = FALSE){
+create_package_igraph <- function(path = ".", include_base = FALSE, directed = TRUE, external = FALSE) {
   
   mapped <- functionMap::map_r_package(path = path, include_base = include_base)
   
+# remove external functions if external == FALSE  
   if (external) {
     node_df <- mapped$node_df
   } else {
     node_df <- mapped$node_df[mapped$node_df$own == TRUE,]
   }
 
-  edge_df <- mapped$edge_df[mapped$edge_df$to %in% unique(node_df$ID),]
-  
-  edge_df <- edge_df[!duplicated(edge_df[,c('from','to')]),] ## include unique edges 
+# remove duplicate vertices
 
-  unique_nodes <- node_df[!duplicated(node_df$ID),]
-
-  igraph_obj <- igraph::graph_from_data_frame(edge_df, directed = directed, vertices = unique_nodes)
+  node_df <- node_df[!duplicated(node_df$ID),]
   
-  igraph::set_vertex_attr(igraph_obj, "exported", value = unique_nodes$exported)
+# remove "_" which often appears as a vertex
+
+  node_df <- node_df[node_df$ID != "_",]
+  
+# order vertices by ID (useful for selecting nodes)  
+
+  node_df <- node_df[order(node_df$ID),]
+  
+# verify that "from" vertices are in node list  
+  edge_df <- mapped$edge_df[mapped$edge_df$to %in% node_df$ID,]
+  
+# verify that "to vertices are in node list  
+  edge_df <- mapped$edge_df[mapped$edge_df$from %in% node_df$ID,]
+
+# eliminate duplicate edges    
+  edge_df <- edge_df[!duplicated(edge_df[,c('from','to')]),] 
+  igraph_obj <- igraph::graph_from_data_frame(edge_df, directed = directed, vertices = node_df)
+  
+  igraph::set_vertex_attr(igraph_obj, "exported", value = node_df$exported)
   
 }
 
